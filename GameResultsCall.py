529d8b427fd5bc7ca7ea00f9a78fd5901124f96d
chasecameron <chasecameron@finfolio.com>
2024-04-27 20:14:52 -0500

    Updated to create files for each game under the home team as they are pulled in

--- GameResultsCall.py
+++ GameResultsCall.py
@@ -1,10 +1,12 @@
 
 import requests
 import json
+import pandas as pd
+import os
 
 
 # Authentication credentials for API request
-headers = {"Authorization": "Your Token"}
+headers = {"Authorization": "Your token here"}
 
 # Convert to readable format
 def jprint(obj):
@@ -34,15 +36,54 @@ def GameResults():
     # Convert json to human readable format
     jprint(response.json())
 
+    df = pd.DataFrame(columns=['GameID', 'Home_Team', 'Away_Team'])
+
+
     # Get list
     data = response.json()
     # Extract data for database
     for d in data:
         home_team = d['home_team']
-        home_points = d['home_points']
+        home_score = d['home_points']
         away_team = d['away_team']
         away_score = d['away_points']
+        away_conference = d['away_conference']
+        if home_score > away_score:
+            outcome = 'W'
+        else:
+            outcome = 'F'
+        margin = home_score - away_score
+        date = d['start_date'][:10]
         game_id = d['id']
+
+        #rw = {'GameID': game_id, 'Home_Team': home_score, 'Away_Team': away_score}
+        #df.loc[len(df)] = rw
+        df = df.append({'GameID': game_id, 'Home_Team': home_score, "Away_Team": away_score}, ignore_index=True)
+
+    df.columns = ['GameID', home_team, away_team]
+
+    # Create a new row from the current game data
+    newFrame = pd.DataFrame(columns=['Opponent', 'Conference', 'Outcome', 'Margin'])
+    newFrame = newFrame.append(
+        {"Opponent": away_team, "Conference": away_conference, "Outcome": outcome, "Margin": margin}, ignore_index=True)
+
+    # Check if the team folder exists, create if not
+    folder_name = os.path.join("Teams", home_team)
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    filename = os.path.join(folder_name, f"{date}.csv")
+    # If the file already exists, load, add the row, save
+    if os.path.exists(filename):
+        existing = pd.read_csv(filename)
+        update = existing.append(newFrame, ignore_index=True)
+        update.to_csv(filename, index=False)
+    # If not, create, add the row, save
+    else:
+        newFrame.to_csv(filename, index=False)
+
+    #df.to_csv('C:/Temp/PandasExport.csv', index=False)
+
     return game_id
 
 
@@ -61,4 +102,4 @@ if __name__ == '__main__':
     test = GameResults()
     print()
     print()
-    GameStatisctics(test)
+    #GameStatisctics(test)
