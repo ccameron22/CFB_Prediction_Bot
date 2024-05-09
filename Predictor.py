7f1f3794c2fff50e7445893f68e62ddf2f89ca06
ccameron22 <cccameron2018@gmail.com>
2024-05-09 11:46:43 -0500

    Created base algorithm to find strength of wins and losses
    Weighting needs revision but works for now

--- Predictor.py
+++ Predictor.py
@@ -5,10 +5,11 @@ import pandas as pd
 team = 'Alabama'
 season = 2023
 
-path = f'Teams/{team}/{season}'
+statsPath = f'Teams/{team}/{season}'
 
 # Read in data
-seasonStats = pd.read_csv(path)
+seasonStats = pd.read_csv(statsPath)
+confStrength = pd.read_csv('ConferenceStrength.csv')
 
 # Find statistics
 
@@ -24,6 +25,23 @@ for value in seasonStats['Outcome']:
         lose += 1
 winRate = round(win/(win + lose), 3)
 
+# Calculate strength of each game
+calculatedGameValues = {}
+for index, row in seasonStats.iterrows():
+    oppCon = row['Conference']
+    marVic = row['Margin']
+    opponent = row['Opponent']
+    test = confStrength["Conference"]
+    if oppCon not in confStrength['Conference'].values:
+        strength = 0.2
+        calculatedGameValues[opponent] = marVic * strength
+    else:
+        for index, line in confStrength.iterrows():
+            test = line["Conference"]
+            if line['Conference'] == oppCon:
+                strength = line['Strength'] * 0.2
+                calculatedGameValues[opponent] = marVic * strength
+print(calculatedGameValues)
 
 # Print results to check validity
 print("Average Margin of Victory: ", averageMargin)
